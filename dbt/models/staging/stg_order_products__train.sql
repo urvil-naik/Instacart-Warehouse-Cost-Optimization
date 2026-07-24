1d00731b87ea302185d140f148b2c88ce4b5c384
urvilnaik22@gmail.com <urvilnaik22@gmail.com>
2026-07-24 21:28:13 +0000

    Type casting and Data quality tests done to staging layer

---
+++ dbt/models/staging/stg_order_products__train.sql
@@ -0,0 +1,6 @@
+select
+    cast(order_id as integer) as order_id,
+    cast(product_id as integer) as product_id,
+    cast(add_to_cart_order as integer) as add_to_cart_order,
+    cast(reordered as boolean) as reordered
+from {{ source('raw', 'order_products__train') }}
