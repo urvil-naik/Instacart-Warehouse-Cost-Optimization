f94fe4a78510263e89c4a335a377764f895d7818
gcnfc 7 <gcnfc7@gmail.com>
2026-08-20 00:36:40 +0000

    Changed data type for reordered to int for aggregation and calculations

--- dbt/models/staging/stg_order_products.sql
+++ dbt/models/staging/stg_order_products.sql
@@ -2,7 +2,7 @@ select
     cast(order_id as integer) as order_id,
     cast(product_id as integer) as product_id,
     cast(add_to_cart_order as integer) as add_to_cart_order,
-    cast(reordered as boolean) as reordered,
+    cast(reordered as integer) as reordered,
     'PRIOR' as eval_set
 from {{ source('raw', 'order_products__prior') }}
 
@@ -12,6 +12,6 @@ select
     cast(order_id as integer) as order_id,
     cast(product_id as integer) as product_id,
     cast(add_to_cart_order as integer) as add_to_cart_order,
-    cast(reordered as boolean) as reordered,
+    cast(reordered as integer) as reordered,
     'TRAIN' as eval_set
 from {{ source('raw', 'order_products__train') }}
